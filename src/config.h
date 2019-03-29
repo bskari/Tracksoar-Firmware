b939ede0ae886a3929e9b051aa5835eaa4ec08a8
Brandon Skari <brandon@skari.org>
2019-03-28 23:53:41 -0600

    Default to empty path
    Path of WIDE2-1 at altitude generates way too much traffic and will likely get you banned from aprs.fi, so default to empty path.

--- src/config.h
+++ src/config.h
@@ -52,11 +52,12 @@
 #define D_CALLSIGN_ID   0
 
 // Digipeating paths:
-// (read more about digipeating paths here: http://wa8lmf.net/DigiPaths/ )
-// The recommended digi path for a balloon is WIDE2-1 or pathless. The default
-// is pathless. Uncomment the following two lines for WIDE2-1 path:
-#define DIGI_PATH1      "WIDE2"
-#define DIGI_PATH1_TTL  1
+// The recommended digi path for a balloon is pathless. A path
+// of WIDE2-1 will likely get you banned from the aprs.fi network
+// with a message of "Seriously bad path". DOn't use it at altitude.
+// Comment these out to leave the path blank.
+//#define DIGI_PATH1      "WIDE2"
+//#define DIGI_PATH1_TTL  1
 
 // APRS comment: this goes in the comment portion of the APRS message. You
 // might want to keep this short. The longer the packet, the more vulnerable
